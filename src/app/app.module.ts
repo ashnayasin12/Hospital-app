c16fe5dbfa95b437a943ddf4b910ef29c1ee40ee
ashnayasin12 <ashnayasin12@gmail.com>
2022-07-09 14:54:40 +0530

    search-department created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
+import { SearchDepartmentComponent } from './search-department/search-department.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrontpageComponent
+    FrontpageComponent,
+    SearchDepartmentComponent
   ],
   imports: [
     BrowserModule,
